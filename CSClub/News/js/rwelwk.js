b3706ef816a3a26fdb08d091efbd5dd56d0c79c5
brockstar17 <brockdyer.git@gmail.com>
2017-06-16 19:47:56 -0400

    Delete rwelwk.js

--- CSClub/News/js/rwelwk.js
+++
@@ -1,5 +0,0 @@
-var names = "<p>Brock Dyer (President), <br>Celine Aubuchon,<br>Nick Devries, <br>Thomas Schaeffer</p>";
-
-var homeName1 = "Brock Dyer (President) <br>Celine Aubuchon <br>";
-
-var homeName2 = "Nick Devries <br>Thomas Schaeffer <br>";
